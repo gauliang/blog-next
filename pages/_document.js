bc669e5479d90098d8bf2f1b1cf918df6568b376
gauliang <gauliang@outlook.com>
2022-06-07 02:09:03 +0800

    Display standalone

--- pages/_document.js
+++ pages/_document.js
@@ -8,9 +8,17 @@ export default function Document() {
       <Head>
         <link href="https://unpkg.com/prismjs@0.0.1/themes/prism-okaidia.css" rel='stylesheet' />
         <link rel="icon" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/favicon.png"></link>
+        <link rel="apple-touch-startup-image" href="/favicon.png"></link>
+        <link rel="manifest" href="/manifest.json" />
+        <meta content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport"/>
+        <meta name="theme-color" content="#000" />
+        <meta name="apple-mobile-web-app-title" content="Blog" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </Head>
       <body>
-        <Main/>
+        <Main />
         <NextScript />
       </body>
     </Html>
