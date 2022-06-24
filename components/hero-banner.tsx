818db6c1c7b721773d7e7cb967bef65692247656
gauliang <gauliang@outlook.com>
2022-06-24 15:26:27 +0800

    Updated Banner

--- components/hero-banner.tsx
+++ components/hero-banner.tsx
@@ -1,7 +1,10 @@
 import { Params } from "next/dist/server/router";
 
 export function HeroBanner({ title, abstract, tag, center }: Params) {
-    return <div className="mx-5 md:mx-20 py-8 md:py-16 space-y-2 md:space-y-5 border-b border-gray-200 dark:border-gray-700">
+    return <div className="
+        mx-0 md:mx-20 py-8 md:py-16 space-y-2 md:space-y-5 md:border-b border-gray-200 dark:border-gray-700
+      bg-slate-100 dark:bg-slate-900/50 md:px-0 px-5 md:bg-transparent md:dark:bg-transparent
+        ">
         <h1 className={`flex text-3xl items-start ${center && `justify-center`}  font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14`}>
             {title}
             {tag && <span className="px-2 mt-1 rounded-tl-md rounded-br-md rounded-tr-md tracking-wider bg-slate-600 text-white ml-2 text-xs font-light">{tag}</span>}
