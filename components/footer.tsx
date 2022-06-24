5071102a37407571d1e6eaf39937245151736763
gauliang <gauliang@outlook.com>
2022-06-24 16:16:39 +0800

    优化分页导航

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,10 +5,10 @@ import { BLOG_LOGO } from '../lib/snippets';
 export default function Footer(props: ScriptProps) {
     return (
         <footer className='border-t dark:border-gray-700'>
-            <div className='container mx-auto flex flex-row justify-center py-16 opacity-50 hover:opacity-100 transition duration-500'>
-                <div className='text-center'>
+            <div className='container mx-auto py-16 opacity-50 hover:opacity-100 transition duration-500'>
+                <div className='flex flex-row justify-center items-center space-x-5'>
                     <Link href={{ pathname: '/' }}>
-                        <a className='flex justify-center mb-3 text-blue-500'>
+                        <a className='flex justify-center text-blue-500 w-8 h-8'>
                             {BLOG_LOGO}
                         </a>
                     </Link>
--- components/post-pagination.tsx
+++ components/post-pagination.tsx
@@ -6,7 +6,7 @@ export function Pagination({ pageSize, total, pageNumber }: Params) {
 
     const totalPage = Math.ceil(total / pageSize)
 
-    const previousInner = <a className={`flex flex-row space-x-4 hover:text-blue-500 ${pageNumber < 2 ? 'pointer-events-none opacity-20' : ''}`}>
+    const previousInner = <a className={`flex flex-row space-x-4 pt-2 border-r dark:border-slate-700 transition-all hover:pl-5  hover:text-blue-500 hover:bg-blue-50/50 dark:hover:bg-blue-500/10 hover:rounded-md ${pageNumber < 2 ? 'pointer-events-none opacity-20' : ''}`}>
         <div className='mt-1'><BsArrowLeftSquare /></div>
         <div className='flex flex-col text-right'>
             <span>上一页</span>
@@ -14,8 +14,8 @@ export function Pagination({ pageSize, total, pageNumber }: Params) {
         </div>
     </a>
 
-    const nextInner = <a className={`flex flex-row space-x-4 hover:text-blue-500 ${pageNumber >= totalPage ? 'pointer-events-none opacity-20' : ''}`}>
-        <div className='flex flex-col text-left'>
+    const nextInner = <a className={`flex flex-row justify-end pt-2 space-x-4 px-2 border-l dark:border-slate-700 transition-all hover:pr-5 hover:text-blue-500 hover:bg-blue-50/50 dark:hover:bg-blue-500/10 hover:rounded-md ${pageNumber >= totalPage ? 'pointer-events-none opacity-20' : ''}`}>
+        <div className='flex flex-col text-right'>
             <span>下一页</span>
             <span className='text-xs opacity-50'>Next</span>
         </div>
@@ -25,14 +25,14 @@ export function Pagination({ pageSize, total, pageNumber }: Params) {
     const previous = pageNumber > 1 ? <Link href={{ pathname: `/blogs/page/${+pageNumber - 1}` }}>{previousInner}</Link> : previousInner
     const next = pageNumber < totalPage ? <Link href={{ pathname: `/blogs/page/${+pageNumber + 1}` }}>{nextInner}</Link> : nextInner
 
-    return <nav className=" border-t dark:border-gray-700 mb-20 py-8 flex flex-row justify-between space-x-8 items-center">
+    return <nav className="grid grid-cols-3 border-t dark:border-gray-700 py-8 transition-all">
         {previous}
-        <div className='flex flex-col items-center space-y-2'>
-            <div className='flex flex-row space-x-1 items-end border-b px-6 pb-1'>
+        <div className='flex flex-col items-center space-y-2 pt-2 md:pt-0'>
+            <div className='flex flex-row space-x-1 items-end md:border-b px-6 pb-1'>
                 <span className='text-2xl leading-none'>{`${pageNumber}`}</span>
                 <span className='text-xs'>/ {Math.ceil(total / pageSize)}</span>
             </div>
-            <div className='text-xs opacity-50'>每页 {pageSize} 条，共 {total} 条</div>
+            <div className='hidden md:block text-xs opacity-50'>每页 {pageSize} 条，共 {total} 条</div>
         </div>
         {next}
     </nav>
